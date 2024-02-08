a614125aefd295ae995484a404a00941cf531f81
syazwaniazahar97 <92834414+syazwaniazahar97@users.noreply.github.com>
2024-02-08 20:06:49 +0800

    create weather app using react and import axios

---
+++ src/Weather.js
@@ -0,0 +1,76 @@
+import react, { useState } from "react";
+import axios from "axios";
+
+export default function Weather() {
+  let [city, setCity] = useState("");
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState({});
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "b35c686ba9565ba0ab254c2230937552";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  function showWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" onChange={updateCity} placeholder="Enter a city.." />
+      <input type="submit" value="Search" />
+    </form>
+  );
+  let footer = (
+    <footer>
+      <a
+        href="https://github.com/syazwaniazahar97/weather-app-react"
+        target="blank"
+      >
+        Open source code
+      </a>
+      , by {" "}
+      <a href="https://github.com/syazwaniazahar97" target="blank">
+        Nur Syazwani Azahar
+      </a>
+    </footer>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+        {footer}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {form}
+        {footer}
+      </div>
+    );
+  }
+}
